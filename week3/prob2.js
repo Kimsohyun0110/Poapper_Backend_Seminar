ac8a9cea3df085c561929b427c062b7ff9555193
김소현 <40044715+Kimsohyun0110@users.noreply.github.com>
2021-11-08 21:27:44 +0900

    Update prob2.js

--- week3/prob2.js
+++ week3/prob2.js
@@ -10,23 +10,19 @@ const server = http.createServer((req, res) => {
     var num = path.split('/');
 
     if(num[1] == 'add'){
-        console.log('hi');
         var add = Number(num[2])+Number(num[3]);
         res.write(`${add}`);
         res.end()
     } 
     else if (num[1] == 'sub'){
-        console.log('hello');
         var sub = Number(num[2])-Number(num[3]);
         res.write(`${sub}`);
         res.end()
     } else if (num[1] == 'mul'){
-        console.log('helll');
         var mul = Number(num[2])*Number(num[3]);
         res.write(`${mul}`);
         res.end()
     } else if (num[1] == 'div'){
-        console.log('heeeee');
         var div = Number(num[2])/Number(num[3]);
         res.write(`${div}`);
         res.end()
